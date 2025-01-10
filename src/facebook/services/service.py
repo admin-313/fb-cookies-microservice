d2e31ce81a9ea38549325fbbb67bf1f02f59a00e
progr <none@mail.com>
2025-01-10 11:52:51 +0200

    abstract class for the webdrivers

---
+++ src/facebook/services/service.py
@@ -0,0 +1,19 @@
+from selenium.webdriver.common.proxy import Proxy
+from abc import ABC, abstractmethod
+
+class WebDriver(ABC):
+    @abstractmethod
+    def get_facebook_data(self) -> str:
+        pass
+    
+    @abstractmethod
+    def set_options(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_user_agent(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_proxy_config(self) -> Proxy:
+        pass
