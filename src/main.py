bef263e1bfc4a4183ae62629c511e396599d3e5f
progr <none@mail.com>
2025-01-10 10:46:22 +0200

    Add a comment for this

--- src/main.py
+++ src/main.py
@@ -5,5 +5,7 @@ from facebook.router import router
 app = FastAPI()
 app.include_router(router=router)
 
+# This two lines of code were made for debug to work properly with FastAPI 
+# docs: https://fastapi.tiangolo.com/tutorial/debugging/
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
