69476f4a7b8bdff0dfe9668a0639c4d5bea39616
progr <none@mail.com>
2025-01-10 12:58:11 +0200

    rename run method name

--- src/facebook/services/service.py
+++ src/facebook/services/service.py
@@ -2,11 +2,12 @@ from selenium.webdriver.common.proxy import Proxy
 from selenium.webdriver.common.options import ArgOptions
 from abc import ABC, abstractmethod
 
+
 class FBWebDriver(ABC):
     @abstractmethod
-    def get_facebook_data(self) -> str:
+    def run_facebook_parser(self) -> str:
         pass
-    
+
     @abstractmethod
     def get_options(self) -> ArgOptions:
         pass
