bfc0d2463245b1ee418b998b550570b5072a8d7b
progr <none@mail.com>
2025-01-10 11:52:27 +0200

    add proxy config

---
+++ src/facebook/configs.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def get_proxy_link() -> str:
+    proxy_ulr: str | None = os.getenv("PROXY_URL")
+    if proxy_ulr:
+        return proxy_ulr
+    else:
+        raise ValueError("The proxy value can't be empty!")
