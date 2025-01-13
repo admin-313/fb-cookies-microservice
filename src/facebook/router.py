568871371f8a3c34dccd1366503ebcea941cd047
w <w@quantum-level.net>
2025-01-13 18:56:32 +0200

    Move object creation to the body of route

--- src/facebook/router.py
+++ src/facebook/router.py
@@ -2,9 +2,9 @@ from fastapi import APIRouter
 from facebook.services.geckodriver.geckodriver_web_driver_impl import GeckodriverFBWebDriverImpl
 
 router = APIRouter()
-driver = GeckodriverFBWebDriverImpl()
 
 @router.get("/parse")
 async def read_parse() -> str:
+    driver = GeckodriverFBWebDriverImpl()
     await driver.run_facebook_parser()
     return ""
