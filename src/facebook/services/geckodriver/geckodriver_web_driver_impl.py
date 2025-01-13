94fe6d3c0cd7b1ff15a69eb8311fb62e1ad6791e
w <w@quantum-level.net>
2025-01-13 14:09:24 +0200

    Comment

--- src/facebook/services/geckodriver/geckodriver_web_driver_impl.py
+++ src/facebook/services/geckodriver/geckodriver_web_driver_impl.py
@@ -1,19 +1,22 @@
+import asyncio
 from selenium import webdriver
 from selenium.webdriver.common.proxy import Proxy, ProxyType
 from facebook.services.service import FBWebDriver
 from facebook.config import FakeUserAgentConfig, ProxyConfig
 
 
-class GeckodriverWebDriverImpl(FBWebDriver):
+class GeckodriverFBWebDriverImpl(FBWebDriver):
     _firefox_driver: webdriver.Firefox
 
-    def run_facebook_parser(self) -> str:
+    async def run_facebook_parser(self) -> str:
         try:
             options: webdriver.FirefoxOptions = self.get_options()
             self._firefox_driver = webdriver.Firefox(options=options)
             self._firefox_driver.get("https://am.i.mullvad.net/")
+            await asyncio.sleep(60)
         finally:
-            self._firefox_driver.quit()
+            if self._firefox_driver:
+                self._firefox_driver.quit()
 
         return ""
 
@@ -22,7 +25,6 @@ class GeckodriverWebDriverImpl(FBWebDriver):
         user_agent: str = self.get_user_agent()
 
         gecko_options: webdriver.FirefoxOptions = webdriver.FirefoxOptions()
-
         gecko_options.proxy = proxy_config
         gecko_options.set_preference(
             name="general.useragent.override", value=user_agent
@@ -34,9 +36,18 @@ class GeckodriverWebDriverImpl(FBWebDriver):
         return FakeUserAgentConfig.get_fake_ua()
 
     def get_proxy_config(self) -> Proxy:
+        # I'm writing this comment here to tell future developer who stumbles upon this that
+        # Neither Chromium nor Geckodriver support SOCKSv5 Private(auth) proxies.
+        # We mad because of this yet we have no other options but to use https at this point.
+        # For some reason browser engines devs don't want to implements this
+        # since on the github of those projects it's already been a while since the
+        # pull requests with private socksv5 additions have been proposed.
+        # I kindly ask you to keep this comment here so noone will spend hours of
+        # his time figuring out how to migrate to socksv5 from http. Thank you for
+        # paying your attention to this project and have a great rest of your day
         proxy_link: str = ProxyConfig.get_proxy_link()
         proxy_config: Proxy = Proxy(
-            {"proxyType": ProxyType.MANUAL, "socksProxy": proxy_link}  # type: ignore
+            {"proxyType": ProxyType.MANUAL, "httpProxy": proxy_link}  # type: ignore
         )
 
         return proxy_config
