2360934bda88bc49251d2d8f5fd5fe66243422e1
w <w@quantum-level.net>
2025-01-13 18:01:00 +0200

    move proxy config creation to config

--- src/facebook/config.py
+++ src/facebook/config.py
@@ -1,19 +1,18 @@
 import os
-
 from fake_useragent import UserAgent
-from dotenv import load_dotenv
-
-load_dotenv()
 
 
 class ProxyConfig:
     @staticmethod
-    def get_proxy_link() -> str:
-        proxy_ulr: str | None = os.getenv("PROXY_URL")
-        if proxy_ulr:
-            return proxy_ulr
-        else:
-            raise ValueError("The proxy value can't be empty!")
+    def get_socks5_proxy_config() -> dict[str, dict[str, str | None]]:
+        socks_proxy: str | None = os.getenv("PROXY_URL")
+        return {
+            "proxy": {
+                "http": socks_proxy,
+                "https": socks_proxy,
+                "no_proxy": "localhost,ENDPOINT",
+            }
+        }
 
 
 class FakeUserAgentConfig:
