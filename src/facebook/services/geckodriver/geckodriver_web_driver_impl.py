6f8435d25010789701fea78b8d4abc596475ce79
progr <none@mail.com>
2025-01-14 12:52:50 +0200

    Move webdriver creation to the separate method

--- src/facebook/services/geckodriver/geckodriver_web_driver_impl.py
+++ src/facebook/services/geckodriver/geckodriver_web_driver_impl.py
@@ -1,5 +1,6 @@
 import asyncio
 from facebook.services.fb_web_driver import FBWebDriver
+from facebook.exceptions import FBWebdriverHasNotBeenInstanciatedException
 from facebook.config import FakeUserAgentConfig, ProxyConfig
 from seleniumwire import webdriver  # type: ignore
 
@@ -9,26 +10,31 @@ class GeckodriverFBWebDriverImpl(FBWebDriver):
 
     async def run_facebook_parser(self) -> str:
         try:
-            options_selenium: webdriver.FirefoxOptions = self.get_selenium_options()
-            options_seleniumwire: dict[str, dict[str, str | None ] | bool] = (
-                self.get_seleniumwire_options()
-            )
-            self._firefox_driver = webdriver.Firefox(
-                seleniumwire_options=options_seleniumwire, options=options_selenium
-            )
-            self._firefox_driver.get("https://api.ipify.org?format=json")
-            await asyncio.sleep(6)
-        except Exception as e:
-            print(e)
+            self._set_options()
+
+            if isinstance(self._firefox_driver, webdriver.Firefox):
+                self._firefox_driver.get("https://api.ipify.org?format=json")
+                await asyncio.sleep(6)
+            else:
+                raise FBWebdriverHasNotBeenInstanciatedException(
+                    "The driver failed to be instanciated"
+                )
+
         finally:
             if self._firefox_driver:
                 self._firefox_driver.quit()
 
         return ""
 
-    def parse_token_from_html(self, html: str) -> str:
-        pass
-    
+    def _set_options(self) -> None:
+        options_selenium: webdriver.FirefoxOptions = self.get_selenium_options()
+        options_seleniumwire: dict[str, dict[str, str | None] | bool] = (
+            self.get_seleniumwire_options()
+        )
+        self._firefox_driver = webdriver.Firefox(
+            seleniumwire_options=options_seleniumwire, options=options_selenium
+        )
+
     def get_selenium_options(self) -> webdriver.FirefoxOptions:
         user_agent: str = self.get_random_user_agent()
 
@@ -43,9 +49,13 @@ class GeckodriverFBWebDriverImpl(FBWebDriver):
 
         return gecko_options
 
-    def get_seleniumwire_options(self) -> dict[str, dict[str, str | None ] | bool]:
+    def get_seleniumwire_options(self) -> dict[str, dict[str, str | None] | bool]:
         proxy_options = self.get_socks5_proxy_config()
-        ssl_options = {"verify_ssl": False, "suppress_connection_errors": True, "accept_untrusted_certs ": True}
+        ssl_options = {
+            "verify_ssl": False,
+            "suppress_connection_errors": True,
+            "accept_untrusted_certs ": True,
+        }
 
         return proxy_options | ssl_options
 
