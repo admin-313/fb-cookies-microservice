416630ec754d84b9926575f663c18d3789940d8c
progr <none@mail.com>
2025-01-15 15:34:24 +0200

    Add json config parser and validator

--- src/facebook/config.py
+++ src/facebook/config.py
@@ -1,6 +1,12 @@
 import os
-from pydantic import BaseModel, ConfigDict
+import json
+from typing import Any
 from fake_useragent import UserAgent
+from pydantic import ValidationError
+from facebook.schemas import JSONFBWebdriverConfig
+from facebook.exceptions import FBWebdriverInvalidConfigProvided
+
+JSON_CONFIG: str = "../config.json"
 
 
 class ProxyConfig:
@@ -23,9 +29,15 @@ class FakeUserAgentConfig:
         return ua.random
 
 
-class JSONFBWebdriverConfig(BaseModel):
-    model_config = ConfigDict(strict=True)
+class GetJSONConfig:
+    @staticmethod
+    def get_json_config() -> dict[str, str]:
+        content: dict[Any, Any]
+        with open(JSON_CONFIG, "r") as json_file:
+            content = json.load(json_file)
 
-    proxy: str
-    user_agent: str
-    cookie: str
+        try:
+            JSONFBWebdriverConfig.model_validate(content)
+            return content
+        except ValidationError as v_e:
+            raise FBWebdriverInvalidConfigProvided(v_e)
