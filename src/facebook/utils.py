afc1a41935fbbdbc14c8f725a6299328e836faf7
progr <none@mail.com>
2025-01-16 11:09:38 +0200

    Bug fix

--- src/facebook/utils.py
+++ src/facebook/utils.py
@@ -1,11 +1,12 @@
 class CookieStringParser:
     @staticmethod
     def parse_cookie_string(cookie_str: str) -> dict[str, str]:
-        pairs: list[str] = cookie_str.split(';')
+        pairs: list[str] = cookie_str.split(";")
         cookie_dict: dict[str, str] = {}
-        
+
         for pair in pairs:
-            key, value = pair.strip().split('=', 1)
-            cookie_dict[key] = value
+            splitted_pair: list[str] | None = pair.split("=") if "=" in pair else None
+            if splitted_pair and len(splitted_pair) == 2:
+                cookie_dict[splitted_pair[0]] = splitted_pair[1]
 
         return cookie_dict
