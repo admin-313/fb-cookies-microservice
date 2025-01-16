a26de229293938c2f7dd250bb43add02737f03f6
progr <none@mail.com>
2025-01-16 13:16:54 +0200

    Add logging to runtime and exceptions and exception handler to a router

--- src/facebook/router.py
+++ src/facebook/router.py
@@ -1,10 +1,63 @@
-from fastapi import APIRouter
-from facebook.services.geckodriver.geckodriver_web_driver_impl import GeckodriverFBWebDriverImpl
+import logging
+from fastapi import APIRouter, HTTPException
+from facebook.services.geckodriver.geckodriver_web_driver_impl import (
+    GeckodriverFBWebDriverImpl,
+)
+from facebook.exceptions import (
+    TokenParseException,
+    Socks5ProxyParseFail,
+    FBWebdriverCouldNotLoginToFb,
+    FBWebdriverHasNotBeenInstanciated,
+    FBWebdriverInvalidConfigProvided,
+)
+
 
 router = APIRouter()
 
+
 @router.get("/parse")
-async def read_parse() -> str:
+async def read_parse():
     driver = GeckodriverFBWebDriverImpl()
-    await driver.run_facebook_parser()
-    return ""
+    try:
+        logging.info("Starting up the driver")
+        await driver.run_facebook_parser()
+        logging.info("Stopping up the driver")
+
+    except TokenParseException as tpe:
+        logging.error(f"Failed to parse token {str(tpe)}")
+
+        raise HTTPException(
+            status_code=500, detail="We couldn't parse the token for you"
+        )
+
+    except Socks5ProxyParseFail as s5ppf:
+        logging.error(f"Failed to get SOCKS5 proxy {str(s5ppf)}")
+
+        raise HTTPException(
+            status_code=500, detail="We couldn't parse the token for you"
+        )
+
+    except FBWebdriverCouldNotLoginToFb as fbwcnlf:
+        logging.error(f"Webdriver failed to login to Facebook {str(fbwcnlf)}")
+
+        raise HTTPException(status_code=500, detail="Webdriver Error")
+
+    except FBWebdriverInvalidConfigProvided as conf_err:
+        logging.error(f"Bad user config: {str(conf_err)}")
+
+        raise HTTPException(status_code=500, detail="Wrong config provided")
+
+    except FBWebdriverHasNotBeenInstanciated as e:
+        logging.critical(f"Internal webdriver issue {str(e)}")
+
+        raise HTTPException(status_code=500, detail="Webdriver Error")
+
+    except Exception as e:
+        logging.critical(f"Unexpected error: {str(e)}")
+
+        raise HTTPException(
+            status_code=500,
+            detail="Unexpected error. Things were never meant to go this way",
+        )
+    finally:
+        logging.info("The /parse thread terminated")
--- src/main.py
+++ src/main.py
@@ -1,11 +1,18 @@
 import uvicorn
+import logging
 from fastapi import FastAPI
 from facebook.router import router
 
+
+logging.basicConfig(level=10)
+
 app = FastAPI()
+logging.info("The app has been started")
 app.include_router(router=router)
+logging.info("The routers have been attached")
 
 # This two lines of code were made for debug to work properly with FastAPI
 # docs: https://fastapi.tiangolo.com/tutorial/debugging/
 if __name__ == "__main__":
+    logging.debug("Uvcorn runs in DEBUG mode!")
     uvicorn.run(app, host="0.0.0.0", port=8000)
