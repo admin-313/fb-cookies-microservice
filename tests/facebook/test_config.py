349147ce4f1210159180b286f54bfd91ab7cc778
progr <none@mail.com>
2025-01-16 15:59:02 +0200

    add adsmanager to tests

--- tests/facebook/test_config.py
+++ tests/facebook/test_config.py
@@ -8,6 +8,7 @@ def test_get_json_config_returns_config_data() -> None:
         "proxy": "socks5://example.com",
         "user_agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0 [ip:36.2.64.67]",
         "cookie": "datr=Iy-BV;ps_n=1;ps_l=1;sb=OoM;c_user=61765;b_user=611566;ar_debug=1;xs=6mP78as7M;fr=12apYWU2EK4zYl0;wd=1600x766;dpr=1.35;usida=eyU1MX0%3D;presence=EDF_7bCC;",
+        "adsmanager_link": "https://dummyinfo.hu"
     }
 
     real_result: JSONFBWebdriverConfig = GetJSONConfig.get_json_config("./tests/test.config.json")
