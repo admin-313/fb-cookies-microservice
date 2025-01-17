155f624eb307ad9894ed380e0692650aa6528432
progr <none@mail.com>
2025-01-17 17:38:15 +0200

    Rewrite js parse exception

--- src/facebook/router.py
+++ src/facebook/router.py
@@ -1,10 +1,11 @@
 import logging
 from fastapi import APIRouter, HTTPException
+from facebook.schemas import ParserResponce
 from facebook.services.geckodriver.geckodriver_web_driver_impl import (
     GeckodriverFBWebDriverImpl,
 )
 from facebook.exceptions import (
-    TokenParseException,
+    FBDriverJSParseException,
     Socks5ProxyParseFail,
     FBWebdriverCouldNotLoginToFb,
     FBWebdriverHasNotBeenInstanciated,
@@ -19,22 +20,22 @@ router = APIRouter()
 async def read_parse():
     driver = GeckodriverFBWebDriverImpl()
     try:
-        logging.info("Starting up the driver")
-        await driver.run_facebook_parser()
-        logging.info("Stopping up the driver")
-
-    except TokenParseException as tpe:
-        logging.error(f"Failed to parse token {str(tpe)}")
+        responce: ParserResponce = await driver.run_facebook_parser()
+        return responce.model_dump()
+    
+    except FBDriverJSParseException as js_ex:
+        logging.error(str(js_ex))
 
         raise HTTPException(
-            status_code=500, detail="We couldn't parse the token for you"
+            status_code=500,
+            detail="Failed to parse the data from the obtained facebook page :c",
         )
 
     except Socks5ProxyParseFail as s5ppf:
         logging.error(f"Failed to get SOCKS5 proxy {str(s5ppf)}")
 
         raise HTTPException(
-            status_code=500, detail="We couldn't parse the token for you"
+            status_code=500, detail="We couldn't parse the proxy for webdriver config"
         )
 
     except FBWebdriverCouldNotLoginToFb as fbwcnlf:
